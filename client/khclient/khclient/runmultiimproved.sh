084a4fb9ec54cefda2f6e0ea9e04e748fa8eb8c1
Chris <chris.a.johnson@gov.ab.ca>
2026-03-01 19:30:16 -0800

    Updated Khclient.  Now will  not crash if server is down or server gives html or garbage response.

--- client/khclient/khclient/runmultiimproved.sh
+++ client/khclient/khclient/runmultiimproved.sh
@@ -1,14 +1,12 @@
-#!/bin/bash
-set -e
-
 CLIENT="./khclientimproved"
-INSTANCES=10
+INSTANCES=26
 LOGDIR="logs"
 
 # absolute path for found file
 FOUND_FILE="$(pwd)/foundNEW.txt"
 
 mkdir -p "$LOGDIR"
+
 CORES=$(nproc)
 
 echo "Detected $CORES CPU cores"
@@ -17,23 +15,15 @@ echo "Launching $INSTANCES khclient instances..."
 for ((i=0; i<INSTANCES; i++))
 do
     CORE=$((i % CORES))
+
     echo "Starting client $i on core $CORE"
 
     taskset -c $CORE \
         $CLIENT -o "$FOUND_FILE" \
         > "$LOGDIR/client_$i.log" 2>&1 &
 
-    sleep 0.15
+    sleep 0.05
 done
 
-sleep 1
 echo "All clients launched."
-
-# Tail all logs so Cloud Batch sees live output
-tail -f "$LOGDIR"/*.log &
-wait -n  # wait until at least one client finishes
-wait     # wait for all to finish
-
-
-
 
